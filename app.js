6524b1a9f81b3daae0313fbcb362022766729dd1
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-09-13 22:44:26 +0900

    Establish model and view base.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
-    
-})
+
+    /**
+     * DISPLAY DRAWINGS
+     */
+
+    const displayDrawings = () => {
+        for (let i = 0; i < displayMe.length; i++) {
+            console.log(displayMe[i].picSrc);
+        }
+    }
+    displayDrawings();
+
+});
--- dataHandler.js
+++ dataHandler.js
@@ -6,7 +6,19 @@ Handles data
 
 (() => {
 
-    window.displayMe = {};
-    displayMe.pictures = [];
+    window.displayMe = [];
+
+    const images = ['./images/Combini.jpg', './images/Refactor.jpg'];
+
+    const generatePictureData = () => {
+        for(let i = 0; i < images.length; i++) {
+            const pic = {};
+            const picSrc = images[i];
+            pic.picSrc = picSrc;
+            displayMe.push(pic);
+        }
+    }
+
+    generatePictureData();
 
 })();
